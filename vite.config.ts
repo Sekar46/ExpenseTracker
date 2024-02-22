7722df5092f268fa033adf71b9024dbbdb1dca88
Chandra Sekar <rebelcs46@gmail.com>
2024-02-22 18:58:22 +0530

    Added config files

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: { '@': path.resolve(__dirname, './src') }
+  },
+  build: {
+    minify: 'esbuild',
+    cssCodeSplit: true,
+    polyfillModulePreload: true,
+    reportCompressedSize: true
+  }
+})
